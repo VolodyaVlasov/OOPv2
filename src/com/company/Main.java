9beab7671ac380837e9c73e43fed10775f056f7a
VolodyaVlasov <ivlasovvladimir@gmail.com>
2021-03-11 01:03:35 +0300

    added Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
     }
+
+    public void pizzaWeight(List<? extends Pizza> list) { // ковариантность
+    }
+
+    public void pizzaPrice(List<? super fourCheeses> list) { //контвариантность
+    }
+
 }
+
+class Pizza {
+}
+
+class fourCheeses extends Pizza {
+}
+
