e3d208611a3b13ab50ff28952234a234766c9205
VolodyaVlasov <ivlasovvladimir@gmail.com>
2021-03-11 01:09:42 +0300

    added Pizza

--- src/com/company/Pizza.java
+++ src/com/company/Pizza.java
@@ -1,24 +1,13 @@
 package com.company;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
-
-    public static void main(String[] args) {
-    }
-
+public class Pizza {
     public void pizzaWeight(List<? extends Pizza> list) { // ковариантность
     }
 
     public void pizzaPrice(List<? super fourCheeses> list) { //контвариантность
     }
-
 }
-
-class Pizza {
-}
-
 class fourCheeses extends Pizza {
 }
-
