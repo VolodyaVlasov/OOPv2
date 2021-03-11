393bf4a473bc633e4628c6cb06432f3af7337778
VolodyaVlasov <ivlasovvladimir@gmail.com>
2021-03-11 21:32:28 +0300

    added Car

---
+++ src/com/company/Car.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Car {
+    final String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public final String getName() {
+        return name;
+    }
+}
