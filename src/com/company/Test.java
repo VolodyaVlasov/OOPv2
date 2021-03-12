cf4582597d4f0733a66527540d4f07208c180224
VolodyaVlasov <ivlasovvladimir@gmail.com>
2021-03-12 14:06:24 +0300

    added Test

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -1,14 +1,35 @@
 package com.company;
 
 public class Test {
-    public void hello(){
+    public void hello() {
         System.out.println("Hello");
+    }
+
+    protected void goodbye() {
+        System.out.println("Goodbye");
+    }
+
+    protected void price() {
+        System.out.println(10);
     }
 }
 
 class deepTest extends Test {
     @Override
-    public void hello() {            //метод публичен в родительском классе А и публичен в его потомке B;
+    public void hello() {//метод публичен в родительском классе Test и публичен в его потомке deepTest.
         System.out.println("Hello from deep");
     }
+
+    @Override
+    public void goodbye() {//метод скрыт в родительском классе Test и публичен в его потомке deepTest.
+        System.out.println("Goodbye");
+    }
+
+    @Override
+    protected void price() { //метод скрыт в родительском классе Test и скрыт в его потомке deepTest.
+        System.out.println(100);
+    }
+
 }
+
+
