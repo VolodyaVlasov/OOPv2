5c57d8090a9f412875597c632f9709c433396cf0
Vladimir <ivlasovvladimir@gmail.com>
2021-05-18 01:13:04 +0300

    update

---
+++ src/com/company/ImplementationInheritance.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class ImplementationInheritance {
+    class HashMap{}
+
+    class LinkedHashMap extends HashMap{}
+}
+
+
+class FacilityInheritance{
+    class Damage{}
+
+    class PhysicalDamage extends Damage {}
+}
